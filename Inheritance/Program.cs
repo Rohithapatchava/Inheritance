2b3a6e62c384745fe9cf4f467f3db6daac4cb04d
Rohithapatchava <60814260+Rohithapatchava@users.noreply.github.com>
2020-02-19 17:49:54 +0530

    Initial commit

---
+++ Inheritance/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    interface Ihello
+    {
+        void getit();
+    }
+    class Doit : Ihello
+    {
+        public void getit()
+        {
+            Console.WriteLine("hello");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Doit d= new Doit();
+            d.getit();
+        }
+    }
+}
